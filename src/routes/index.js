33e3d97a40ef906fd16ecf5439f620c97437abbd
Himanshu Sharma <hishrma00@gmail.com>
2024-05-03 06:46:18 +0530

    add-nodemailer

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
+// import { mailer } from "../utils/mailer";
 const express = require("express");
 const foodItemController = require("../controllers/foodItem");
+const nodemailer = require("nodemailer");
+const mailer = require("../utils/mailer");
 
 const router = express.Router();
 
@@ -13,6 +16,25 @@ router.get("/getFoodItems", async (req, res) => {
   } catch (error) {
     console.error("Error ---> ", error);
     res.status(500).send({ success: false, msg: "Error fetching data" });
+  }
+});
+
+// https://myaccount.google.com/apppasswords
+router.post("/email", async (req, res) => {
+  try {
+    let { name, email, mobile, msg } = req.body;
+
+    let htmlbody = `<strong>Name:</strong> ${name} <br/>
+    <strong>Email:</strong> ${email} <br/>
+    <strong>Mobile:</strong> ${mobile} <br/>
+    <strong>Message:</strong> ${msg} <br/>
+    `;
+
+    let result = await mailer(htmlbody);
+    // let result = {}
+    return res.send({ ...result });
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
   }
 });
 
---
+++ src/utils/mailer.js
@@ -0,0 +1,39 @@
+const nodemailer = require("nodemailer");
+
+const mailer = async (
+  //   text = "Hello world",
+  htmlbody = "<b>Hello world?</b>",
+  subject = "Query From PlatterPulse!",
+  sendTo = [ "hishrma02@gmail.com", 'kapilkumar568926@gmail.com' ]
+) => {
+  try {
+    let config = {
+      service: "gmail",
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_APP_PASS,
+      },
+    };
+
+    let transporter = nodemailer.createTransport(config);
+
+    let message = {
+      from: process.env.GMAIL_USER,
+      to: sendTo,
+      subject: subject,
+      //   text,
+      html: htmlbody,
+    };
+
+    let info = await transporter.sendMail(message);
+    return {
+      msg: "Email sent",
+      info: info.messageId,
+      //   preview: nodemailer.getTestMessageUrl(info),
+    };
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+module.exports = mailer;
